50b762e66adcbd5c36b6d082fa07da54f06b2a9b
srinath1997gr <srinath1997.gr@gmail.com>
2023-11-21 10:30:29 -0700

    DSA practice

---
+++ Stacks and Queues/GeeksForGeeks/MinElementinStack.java
@@ -0,0 +1,27 @@
+package GeeksForGeeks;
+
+
+public class MinElementinStack {
+
+    public void push(int a,Stack<Integer> s)
+	{
+	    //add code here.
+	}
+	public int pop(Stack<Integer> s)
+        {
+            //add code here.
+	}
+	public int min(Stack<Integer> s)
+        {
+           //add code here.
+	}
+	public boolean isFull(Stack<Integer>s, int n)
+        {
+           //add code here.
+	}
+	public boolean isEmpty(Stack<Integer>s)
+        {
+           //add code here.
+	}
+    
+}
